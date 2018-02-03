2f6490e7d992242e70922baaa893f26c3a57e33c
Ian <ian@right.here>
2018-02-03 23:41:17 +0000

    Delete the unsed PreDeuce implementations

--- src/main/java/com/paul/teventis/game/TennisScore.java
+++ src/main/java/com/paul/teventis/game/TennisScore.java
@@ -63,56 +63,8 @@ class FifteenLove extends PreDeuce {
     public FifteenLove(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
 }
 
-class FifteenFifteen extends PreDeuce {
-    public FifteenFifteen(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class FifteenThirty extends PreDeuce {
-    public FifteenThirty(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class FifteenForty extends PreDeuce {
-    public FifteenForty(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class ThirtyForty extends PreDeuce {
-    public ThirtyForty(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class ThirtyThirty extends PreDeuce {
-    public ThirtyThirty(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class FortyThirty extends PreDeuce {
-    public FortyThirty(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class ThirtyFifteen extends PreDeuce {
-    public ThirtyFifteen(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class FortyFifteen extends PreDeuce {
-    public FortyFifteen(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class ThirtyLove extends PreDeuce {
-    public ThirtyLove(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class FortyLove extends PreDeuce {
-    public FortyLove(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
 class LoveFifteen extends PreDeuce {
     public LoveFifteen(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class LoveThirty extends PreDeuce {
-    public LoveThirty(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
-}
-
-class LoveForty extends PreDeuce {
-    public LoveForty(String playerOneScore, String playerTwoScore) { super(playerOneScore, playerTwoScore); }
 }
 
 
